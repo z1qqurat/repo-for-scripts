62180457adcc00f00b7b626f1e11c3a1c0152492
Teodor <39533780+z1qqurat@users.noreply.github.com>
2023-10-30 18:10:51 +0200

    RpRadio.lua v1.0

--- RpRadio.lua
+++ RpRadio.lua
@@ -7,10 +7,10 @@ local SE = require 'lib.samp.events'
 local inicfg = require 'inicfg'
 local cfg = inicfg.load({
     settings = {
-	state = true,
+	  state = true,
     seeDoMsg = "/seedo Сообщает что-по рации",
     meMsg = "/me взял рацию тактического пояса",
-    radioMsg = " {put_your_nickname_here}%[(%d+)%]: ",  -- {put_your_nickname_here} Example: Volodia_Patlatuk%[(%d+)%]
+    radioMsg = "",  -- {put_your_nickname_here} Example: Volodia_Patlatuk%[(%d+)%]
     hideseeme = true, -- true = hide (( Сообщение отправлено )) message on /seeme from command
     },
 }, "RpRadio.ini")
@@ -21,8 +21,12 @@ function main()
   if not isSampfuncsLoaded() or not isSampLoaded() then return end
   while not isSampAvailable() do wait(100) end
 
-  sampRegisterChatCommand('rprad', toggleStatus)
+  if cfg.settings.radioMsg == "" then
+    setRadioMsg()
+  end
 
+  sampRegisterChatCommand('rprad', toggleStatus)
+  sampRegisterChatCommand('rprad', toggleStatus)
   while true do
     wait(0)
     if send_radio then
@@ -50,6 +54,12 @@ function SE.onServerMessage(color, text)
     end
   end
   if send_radio and cfg.settings.hideseeme and text == " (( Сообщение отправлено ))" then return false end
+end
+
+function setRadioMsg()
+	local _, myID = sampGetPlayerIdByCharHandle(PLAYER_PED)
+  cfg.settings.radioMsg = sampGetPlayerNickname(myID).."%[(%d+)%]"
+  inicfg.save(cfg, "RpRadio")
 end
 
 function notf(msg)
